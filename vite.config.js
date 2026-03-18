ae901391042e8a9996e2621c5143ab6007dc9142
Marko <datathrills@gmail.com>
2026-03-18 16:41:21 +0100

    Add deployment config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+   import { defineConfig } from 'vite'
+   import { resolve } from 'path'
+
+   export default defineConfig({
+     base: '/my-first-website/',
+     build: {
+       rollupOptions: {
+         input: {
+           main: resolve(__dirname, 'index.html'),
+           about: resolve(__dirname, 'about.html'),
+           projects: resolve(__dirname, 'projects.html'),
+         },
+       },
+     },
+   })
